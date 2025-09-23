d29685e6fcc573d4abfad757f5b429397b56bf47
Elmar Abdullayev <elmar.abdullayev@hitec-hamburg.de>
2025-09-24 00:46:01 +0200

    commit

--- servers/uvicorn.py
+++ servers/uvicorn.py
@@ -24,22 +24,38 @@ def save_result(filename, data):
             writer.writeheader()
         writer.writerows(data)
 
+@uvicorn_api.post("/save_result")
+async def get_async_function():
+    start = datetime.now()
+    await async_test()
+    end = datetime.now()
+    duration_ms = (end - start).total_seconds() * 1000
+    result = [{"server": "uvicorn",
+                "type": "async",
+                "duration_ms": duration_ms,
+                "durchschnitt": "",
+                "status": "",
+                "requests": ""}]
+    save_result("benchmark_results.csv", result)
+    return result
+
 
 async def async_test():
     print("This is just test.")
 
 
-@uvicorn_api.post("/get_async_uvicorn_function")
-async def get_async_function(req:RequestModel):
+@uvicorn_api.get("/get_async_uvicorn_function")
+async def get_async_function():
     start = datetime.now()
     await async_test()
     end = datetime.now()
-    print(req)
     duration_ms = (end - start).total_seconds() * 1000
     result = [{"server": "uvicorn",
                "type": "async",
-               "requests":req.req_number,
-               "duration_ms": duration_ms}]
+               "duration_ms": duration_ms,
+               "durchschnitt":"",
+               "status":"",
+               "requests":""}]
     save_result("benchmark_results.csv", result)
     return result
 
@@ -48,15 +64,15 @@ def sync_test():
     print("This is just test.")
 
 
-@uvicorn_api.post("/get_sync_uvicorn_function")
-def get_sync_function (req:RequestModel):
+@uvicorn_api.get("/get_sync_uvicorn_function")
+def get_sync_function ():
     start = datetime.now()
     sync_test()
     end = datetime.now()
     duration_ms = (end - start).total_seconds() * 1000
     result = [{"server": "uvicorn",
                "type": "sync",
-               "requests":req.req_number,
-               "duration_ms": duration_ms}]
+               "duration_ms": duration_ms,
+               }]
     save_result("benchmark_results.csv", result)
     return result
